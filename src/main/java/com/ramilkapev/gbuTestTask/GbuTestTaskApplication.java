6ff4ca35bb295eb090eb8c4e67b9900e2d0cc95a
5cr1p7 <kapjov0@yandex.ru>
2022-10-01 02:47:18 +0300

    Initial commit

---
+++ src/main/java/com/ramilkapev/gbuTestTask/GbuTestTaskApplication.java
@@ -0,0 +1,13 @@
+package com.ramilkapev.gbuTestTask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GbuTestTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GbuTestTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ramilkapev/gbuTestTask/GbuTestTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ramilkapev.gbuTestTask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GbuTestTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
